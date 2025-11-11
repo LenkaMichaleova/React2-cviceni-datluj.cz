b74b9d8027273ba44c4c6397551d073d6d1a734d
Lenka Michaleová <michaleova.lenka@gmail.com>
2025-11-11 15:27:57 +0100

    The user can type the word displayed on the screen.

--- src/components/Wordbox/index.tsx
+++ src/components/Wordbox/index.tsx
@@ -1,16 +1,25 @@
-import React, { useState } from 'react';
-import './style.css';
+import React, { useEffect, useState } from "react";
+import "./style.css";
 
 interface IWordboxProp {
   word: string;
 }
 
-const Wordbox : React.FC<IWordboxProp> = ({ word }) => {
-  const [lettersLeft] = useState<string>(word);  
-  
-  return (
-    <div className="wordbox">{lettersLeft}</div>
-  );
+const Wordbox: React.FC<IWordboxProp> = ({ word }) => {
+  const [lettersLeft, setLetersLeft] = useState<string>(word);
+
+  const handleKeyUp = (e: KeyboardEvent) => {
+    if (e.key === lettersLeft[0]) {
+      setLetersLeft((prev) => prev.slice(1));
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("keyup", handleKeyUp);
+    return () => document.removeEventListener("keyup", handleKeyUp);
+  }, [lettersLeft]);
+
+  return <div className="wordbox">{lettersLeft}</div>;
 };
 
 export default Wordbox;
